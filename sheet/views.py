dfaa200ad026c157104b9550686afe3a0ac3b000
johnthagen <johnthagen@gmail.com>
2016-04-20 18:51:13 -0400

    Validate level up rolls.

--- sheet/views.py
+++ sheet/views.py
@@ -1,3 +1,4 @@
+from django.forms import ValidationError
 from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 
@@ -21,14 +22,25 @@ def level_up(request: HttpRequest, character_id: int) -> HttpResponse:
     if request.method == 'POST':
         level_up_form = LevelUpForm(request.POST)
         if level_up_form.is_valid():
-            # TODO: Validate rolls are under maximum value.
-            print('VALID!', level_up_form.cleaned_data['hd_roll'],
-                  level_up_form.cleaned_data['md_roll'],
-                  level_up_form.cleaned_data['sd_roll'], level_up_form.cleaned_data['ad_roll'],
-                  level_up_form.cleaned_data['selected_attribute'])
+            character = get_object_or_404(Character, pk=character_id)
+
+            hd_roll = level_up_form.cleaned_data['hd_roll']
+            if hd_roll > character.cls.hd:
+                raise ValidationError('HD roll higher than HD: {}>{}'.format(
+                    hd_roll, character.cls.hd))
+
+            md_roll = level_up_form.cleaned_data['md_roll']
+            if md_roll > character.cls.md:
+                raise ValidationError('MD roll higher than MD: {}>{}'.format(
+                    md_roll, character.cls.md))
+
+            sd_roll = level_up_form.cleaned_data['sd_roll']
+            if sd_roll > character.cls.sd:
+                raise ValidationError('SD roll higher than SD: {}>{}'.format(
+                    sd_roll, character.cls.sd))
 
             new_level_up = level_up_form.save(commit=False)
-            new_level_up.character = get_object_or_404(Character, pk=character_id)
+            new_level_up.character = character
             new_level_up.save()
 
             return HttpResponseRedirect('/sheet/{}/stats/'.format(character_id))
