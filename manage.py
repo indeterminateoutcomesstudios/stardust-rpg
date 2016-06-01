a078ab5a69a302ca07f3aea709e54812efea20da
johnthagen <johnthagen@gmail.com>
2016-05-31 20:36:20 -0400

    Refactor main function.

--- manage.py
+++ manage.py
@@ -3,9 +3,12 @@
 import os
 import sys
 
-if __name__ == '__main__':
+from django.core.management import execute_from_command_line
+
+
+def main() -> None:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
-    from django.core.management import execute_from_command_line
-
     execute_from_command_line(sys.argv)
+
+if __name__ == '__main__':
+    main()
