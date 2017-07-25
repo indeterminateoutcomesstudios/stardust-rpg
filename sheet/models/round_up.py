66065fa6b55d15f4c27f883c05966e562ba8a928
johnthagen <johnthagen@gmail.com>
2017-07-25 16:42:01 -0400

    Fix round_up usage to allot for number of digits to be passed in

--- sheet/models/round_up.py
+++ sheet/models/round_up.py
@@ -1,6 +1,6 @@
 from numbers import Real
 
 
-def round_up(num: Real) -> int:
+def round_up(num: Real, num_digits: int=None) -> int:
     """Add a small epsilon in order to force .5 values to round up."""
-    return round(num + 1e-8)
+    return round(num + 1e-8, num_digits)
