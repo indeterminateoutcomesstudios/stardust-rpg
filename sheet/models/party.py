d5623540b6791fafe61acfafd2b424ef3bb2d6f2
johnthagen <johnthagen@gmail.com>
2017-07-31 18:47:16 -0400

    Add Roll20 URL campaign links

--- sheet/models/party.py
+++ sheet/models/party.py
@@ -5,5 +5,9 @@ class Party(models.Model):
     name = models.CharField(max_length=25)
     roll20_campaign_id = models.PositiveIntegerField(default=0)
 
+    @property
+    def roll20_url(self) -> str:
+        return f'https://app.roll20.net/campaigns/details/{self.roll20_campaign_id}'
+
     def __str__(self):
         return f'{self.name}: {self.roll20_campaign_id}'
